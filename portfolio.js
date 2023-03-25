45f7924c803fb9cf1ddd5a13063a2d371704b572
ShrutiGupta-webdev <shrutirgupta.srg@gmail.com>
2023-03-25 19:17:56 +0530

    push2

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+let display = () => {
+
+    let resnav= document.querySelector(".list");
+    resnav.style.display="block";
+  console.log("working")
+  let hambtn = document.querySelector(".hambtn1");
+   hambtn.style.display="none";
+  }
+
+  let Remove = () => {
+    let rem = document.querySelector(".list");
+    rem.style.display="none";
+    let hambtn = document.querySelector(".hambtn1");
+   hambtn.style.display="block";
+    console.log("working")
+  
+  }
